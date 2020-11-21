2e4d68c2c4f1264134c05e38b7b3de99b047f8af
Nenad Smontara <nsmontar@gmail.com>
2020-11-21 10:06:07 +0100

    Scaffolded Student Model

---
+++ AddProjectPackages.cmd
@@ -0,0 +1,6 @@
+dotnet add package Microsoft.EntityFrameworkCore.SQLite
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.Extensions.Logging.Debug
