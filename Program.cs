4b34833f3734262e8baef395cc467222b2ded5f3
Nenad Smontara <nsmontar@gmail.com>
2020-11-21 12:12:18 +0100

    Seeded the DB with test data

--- Program.cs
+++ Program.cs
@@ -28,8 +28,8 @@ namespace ContosoUniversity
                 try
                 {
                     var context = services.GetRequiredService<SchoolContext>();
-                    context.Database.EnsureCreated();
-                    // DbInitializer.Initialize(context);
+                    // context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
