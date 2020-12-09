075ce584f8bceec6bc741dab9e8c6c0731b63a9f
Nenad Smontara <nsmontar@gmail.com>
2020-12-09 22:13:16 +0100

    Added a tracking property to Department, updated the database, scaffolded the Department razor pages, updated the Edit page and page model

---
+++ Migrations/20201209173811_RowVersioni.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class RowVersioni : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Department",
+                type: "BLOB",
+                rowVersion: true,
+                nullable: true);
+            
+            migrationBuilder.Sql(
+            @"
+                UPDATE Department
+                SET RowVersion = randomblob(8)
+            ");
+
+            migrationBuilder.Sql(
+            @"
+                CREATE TRIGGER SetRowVersionOnUpdate
+                AFTER UPDATE ON Department
+                BEGIN
+                    UPDATE Department
+                    SET RowVersion = randomblob(8)
+                    WHERE rowid = NEW.rowid;
+                END
+            ");
+
+            migrationBuilder.Sql(
+            @"
+                CREATE TRIGGER SetRowVersionOnInsert
+                AFTER INSERT ON Department
+                BEGIN
+                    UPDATE Department
+                    SET RowVersion = randomblob(8)
+                    WHERE rowid = NEW.rowid;
+                END
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Department");
+        }
+    }
+}
